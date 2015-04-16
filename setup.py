49c3c969ec044e1364f2301bbba162c6aa1146ff
Aaron Duke <aaron@0ptimus.com>
2015-04-16 12:26:36 -0400

    Preference for setuptools.

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
+install_requires = []
+
 setup(name='dstk',
       version='0.50',
       py_modules=['dstk'],
       author='Pete Warden',
       author_email='pete@petewarden.com',
-      url='http://www.datasciencetoolkit.org/'
+      url='http://www.datasciencetoolkit.org/',
+      install_requires=install_requires
       )
